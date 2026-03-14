8a38ea2cfad3867d4a719e2462aa60b3477d88e1
Jero457 <jvargasestrada62@hotmail.com>
2026-03-14 15:40:44 -0500

    Cambios hasta el 12/03

---
+++ IUSHmath.h project/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+#include "IUSHmath.h";
+using namespace std;
+
+int main() {
+	cout << setprecision(2) << fixed;
+	double n1 = 2.5, n2 = -5.0;
+	double n3 = 2, n4 = 5;
+	cout << "Suma       " << n1 << " + " << n2 << " = " << Sum(n1, n2) << endl;
+	cout << "Diferencia " << n1 << " - " << n2 << " = " << Dif(n1, n2) << endl;
+	cout << "Producto   " << n1 << " * " << n2 << " = " << Pro(n1, n2) << endl;
+	cout << "Cociente   " << n1 << " / " << n2 << " = " << Coc(n1, n2) << endl;
+	cout << "Valor Absoluto " << n1 << " = " << Vabs(n1) << endl;
+	cout << "Valor Absoluto " << n2 << " = " << Vabs(n2) << endl;
+	cout << "Mayor entre " << n1 << " y " << n2 << ": " << May(n1, n2) << endl;
+	cout << "Menor entre " << n3 << " y " << n4 << ": " << Men(n3, n4) << endl;
+	cout << n3 << " elevado a " << n4 << ": " << Pot(n3, n4) << endl;
+	return 0;
+}
